4533885cb885f7c3fed13a1db0246b9dc3cc959c
WillForshaw <willforshaw4@gmail.com>
2025-08-01 22:02:06 +0100

    coin_stats() -- Function to collect key market data for a list of different coins (hedera not working currently)

--- crypto-portfolio-tracker/crypto-portfolio-tracker/main.py
+++ crypto-portfolio-tracker/crypto-portfolio-tracker/main.py
@@ -11,6 +11,31 @@ if __name__ == "__main__":
 
 
 
+# Another function using a different endpoint to find price, market cap and volume. 
+from dotenv import load_dotenv
+import os 
+import requests
+load_dotenv()
+api_key  = os.getenv("CG_API_KEY")
+
+def coin_stats(coinID, currency):
+
+        url = "https://api.coingecko.com/api/v3/coins/markets"
+        headers = {"x-cg-demo-api-key": api_key}
+
+        params = {
+        'ids': coinID,          
+        'vs_currency': currency   
+    }
+
+        response = requests.get(url, headers=headers, params=params)
+
+        return response.json()
+
+coins = ["bitcoin", "hedera", "solana"]
+
+for coin in coins:                          
+     print(coin_stats(coin, "gbp"))        # Applies function to multiple coins 
 
 
 
